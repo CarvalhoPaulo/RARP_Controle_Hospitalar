39e4a8007c21f58df5103ef813ed172132b635d2
Paulo Henrique Carvalho <paulo200865@hotmail.com>
2017-05-09 00:51:28 -0300

    Criado controller abstrado da tela de manutenção

--- RARP_Controle_Hospitalar/src/br/com/rarp/view/main/scnManutencao/ManutencaoController.java
+++ RARP_Controle_Hospitalar/src/br/com/rarp/view/main/scnManutencao/ManutencaoController.java
@@ -1,43 +1,52 @@
 package br.com.rarp.view.main.scnManutencao;
 
-import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import br.com.rarp.annotations.Coluna;
-import br.com.rarp.annotations.Objeto;
-import br.com.rarp.utils.Utilitarios;
+import br.com.rarp.control.interfaces.Manutencao;
+import br.com.rarp.model.EntradaPaciente;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
-public class ManutencaoController extends Application implements Initializable {
+public abstract class ManutencaoController extends Application implements Initializable, Manutencao {
 
 	private FXMLLoader loader;
-	private static Stage stage;
-	private Class classe;
-	@FXML private TableView tvMovimentacao;
+	private Stage stage;
+	@FXML private TableView<EntradaPaciente> tvManutencao;
 	
-	public ManutencaoController(Class classe) {
+	@FXML private Button btnPesquisar;
+	@FXML private Button btnInserir;
+	@FXML private Button btnAlterar;
+	@FXML private Button btnVisualizar;
+	
+	public ManutencaoController() {
 		loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("Manutencao.fxml"));
+		loader.setLocation(getClass().getResource("../Manutencao.fxml"));
 		loader.setController(this);
-		this.classe = classe;
 	}
 	
-	private void configurarTela() {
-		for(Field f: classe.getDeclaredFields()) {
-			if (f.getDeclaredAnnotation(Objeto.class) != null) {
-				for(Field c: Utilitarios.getColunas(f.getType())) {
-					TableColumn tc = new TableColumn<>(c.getDeclaredAnnotation(Coluna.class).descricao());
-					tvMovimentacao.getColumns().add(tc);
-				}
-			}
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		try {
+			prepararTela();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void voltar() {
+		try {
+			this.finalize();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
@@ -45,6 +54,38 @@ public class ManutencaoController extends Application implements Initializable {
 	public void start(Stage stage) throws Exception {
 		stage.setScene(new Scene(loader.load()));
 		this.setStage(stage);
+	}
+	
+	public Button getBtnPesquisar() {
+		return btnPesquisar;
+	}
+
+	public void setBtnPesquisar(Button btnPesquisar) {
+		this.btnPesquisar = btnPesquisar;
+	}
+
+	public Button getBtnInserir() {
+		return btnInserir;
+	}
+
+	public void setBtnInserir(Button btnInserir) {
+		this.btnInserir = btnInserir;
+	}
+
+	public Button getBtnAlterar() {
+		return btnAlterar;
+	}
+
+	public void setBtnAlterar(Button btnAlterar) {
+		this.btnAlterar = btnAlterar;
+	}
+
+	public Button getBtnVisualizar() {
+		return btnVisualizar;
+	}
+
+	public void setBtnVisualizar(Button btnVisualizar) {
+		this.btnVisualizar = btnVisualizar;
 	}
 
 	public FXMLLoader getLoader() {
@@ -63,9 +104,12 @@ public class ManutencaoController extends Application implements Initializable {
 		this.stage = stage;
 	}
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		configurarTela();	
+	public TableView<EntradaPaciente> getTvManutencao() {
+		return tvManutencao;
+	}
+
+	public void setTvManutencao(TableView<EntradaPaciente> tvManutencao) {
+		this.tvManutencao = tvManutencao;
 	}
 
 }
