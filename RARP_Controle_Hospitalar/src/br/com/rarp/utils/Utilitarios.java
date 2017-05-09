430a42b53a50caae423c748882420a75da5c2ea9
Paulo Henrique Carvalho <paulo200865@hotmail.com>
2017-05-09 00:52:21 -0300

    Criadoo a função erro e atencao.

--- RARP_Controle_Hospitalar/src/br/com/rarp/utils/Utilitarios.java
+++ RARP_Controle_Hospitalar/src/br/com/rarp/utils/Utilitarios.java
@@ -1,43 +1,20 @@
 package br.com.rarp.utils;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import com.thoughtworks.qdox.parser.structs.FieldDef;
-
-import br.com.rarp.annotations.Coluna;
-import br.com.rarp.annotations.Descricao;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class Utilitarios {
-	public static List<Field> getColunas(Class classe) {
-		List<Field> colunas = new ArrayList<>();
-		if(classe.getSuperclass() != Object.class)
-			colunas = getColunas(classe.getSuperclass());
-		for(Field f: classe.getDeclaredFields()) {
-			if(f.getDeclaredAnnotation(Coluna.class) != null) {
-				if(f.getType().isPrimitive() || f.getType() == String.class || f.getType() == Date.class) {
-					colunas.add(f);
-				} else {
-					colunas.add(getDescricao(f.getType()));
-				}
-			}
-		}
-		return colunas;
+	public static void atencao(String message) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setContentText(message);
+		alert.setHeaderText("Atenção");
+		alert.showAndWait();
 	}
 	
-	private static Field getDescricao(Class classe) {
-		if(classe.getSuperclass() != Object.class)
-			return getDescricao(classe.getSuperclass());
-		for(Field f: classe.getDeclaredFields()) {
-			if(f.getType().isPrimitive() || f.getType() == String.class || f.getType() == Date.class) {
-				if(f.getDeclaredAnnotation(Descricao.class) != null)
-					return f;
-			} else {
-				return getDescricao(f.getType());
-			}
-		}
-		return null;
+	public static void erro(String message) {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setContentText(message);
+		alert.setHeaderText("Erro");
+		alert.showAndWait();
 	}
 }
