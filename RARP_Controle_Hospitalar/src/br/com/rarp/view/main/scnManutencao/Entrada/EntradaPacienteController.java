8780ff77bab6a8ea3e09b8fef8bcf2746db612e0
Paulo Henrique Carvalho <paulo200865@hotmail.com>
2017-05-09 00:44:06 -0300

    Classe que herda de ManutencaoControlle e implementa a tela de manuteção

---
+++ RARP_Controle_Hospitalar/src/br/com/rarp/view/main/scnManutencao/Entrada/EntradaPacienteController.java
@@ -0,0 +1,47 @@
+package br.com.rarp.view.main.scnManutencao.Entrada;
+
+import br.com.rarp.model.EntradaPaciente;
+import br.com.rarp.view.main.scnManutencao.ManutencaoController;
+import br.com.rarp.utils.Utilitarios;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class EntradaPacienteController extends ManutencaoController {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void preparaTela() {
+		TableColumn<EntradaPaciente, String> codigo = new TableColumn<>("Código");
+		codigo.setCellValueFactory(new PropertyValueFactory<>("codigo"));
+		TableColumn<EntradaPaciente, String> data = new TableColumn<>("Data");
+		data.setCellValueFactory(new PropertyValueFactory<>("data"));
+		TableColumn<EntradaPaciente, String> hora = new TableColumn<>("Hora");
+		hora.setCellValueFactory(new PropertyValueFactory<>("hora"));
+		TableColumn<EntradaPaciente, String> paciente = new TableColumn<>("Paciente");
+		paciente.setCellValueFactory(new PropertyValueFactory<>("paciente"));
+		
+		getTvManutencao().getColumns().addAll(codigo, data, hora, paciente);
+		
+	}
+
+	@Override
+	public void pesquisar() {
+		Utilitarios.atencao("Função do botão Pesquisar");
+	}
+
+	@Override
+	public void inserir() {
+		Utilitarios.atencao("Função do botão Inserir");
+	}
+
+	@Override
+	public void alterar() {
+		Utilitarios.atencao("Função do botão Alterar");
+	}
+
+	@Override
+	public void visualizar() {
+		Utilitarios.atencao("Função do botão Visualizar");
+	}
+
+}
