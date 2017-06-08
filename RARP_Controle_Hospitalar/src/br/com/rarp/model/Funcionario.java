439e159886f27165444a599a3054e31ed582f514
Renatogsilva <silvarenato180@gmail.com>
2017-06-07 21:53:40 -0300

    Coloquei comentário na classe funcionário

--- RARP_Controle_Hospitalar/src/br/com/rarp/model/Funcionario.java
+++ RARP_Controle_Hospitalar/src/br/com/rarp/model/Funcionario.java
@@ -1,32 +1,38 @@
 package br.com.rarp.model;
 
-import java.util.Date;
 import java.util.Currency;
+import java.util.Date;
 
 public class Funcionario extends PessoaFisica {
+
+	// Primeiro commit testando
 
 	private Date dtUltimaAdmissao;
 	private Currency salarioContratual;
 	private Cargo cargo;
+
 	public Date getDtUltimaAdmissao() {
 		return dtUltimaAdmissao;
 	}
+
 	public void setDtUltimaAdmissao(Date dtUltimaAdmissao) {
 		this.dtUltimaAdmissao = dtUltimaAdmissao;
 	}
+
 	public Currency getSalarioContratual() {
 		return salarioContratual;
 	}
+
 	public void setSalarioContratual(Currency salarioContratual) {
 		this.salarioContratual = salarioContratual;
 	}
+
 	public Cargo getCargo() {
 		return cargo;
 	}
+
 	public void setCargo(Cargo cargo) {
 		this.cargo = cargo;
 	}
-	
-	
 
 }
